c5ca4cbcf31acc96f5cc3d72d9e2e8d1fce300a5
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-05 21:08:31 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -29,7 +29,7 @@ class SongsController < ApplicationController
     if !!params["artist_id"]
       @artist = Artist.find_by(id: params["artist_id"] )
       if !@artist
-        redirect_to artists_path(Artist.all)
+        redirect_to '/artists'
       end
     end
   end
